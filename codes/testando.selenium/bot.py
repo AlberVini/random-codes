c7e43310d3b7c5006c4ef4726a904a2f3962daa3
AlberVini <viiniciusalberto@gmail.com>
2021-08-19 02:04:51 -0300

    testes selenium 19/08

---
+++ codes/testando.selenium/bot.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+
+browser = webdriver.Chrome(r'D:\Vini\Documents\My_Repository\random_cods\codes\testando.selenium\chromedriver.exe')
+
+browser.get("https://www.google.com/")
+
+input('')
