489516344fd0970ad3f871d96a7407e55b6530e4
AlberVini <viiniciusalberto@gmail.com>
2021-08-26 22:16:18 -0300

    edit selenium code

--- codes/testando_selenium/bot.py
+++ codes/testando_selenium/bot.py
@@ -8,4 +8,8 @@ browser.get("https://www.google.com/")
 
 input_search = browser.find_element_by_xpath("//input[@type='text']")
 input_search.send_keys('python')
+# browser.find_element_by_xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]").click()
 input_search.send_keys(Keys.RETURN)
+
+click_search = browser.find_element_by_xpath('//*[@id="rso"]/div[1]/div/div/div/div/div/div/div[1]/a/h3')
+click_search.click()
